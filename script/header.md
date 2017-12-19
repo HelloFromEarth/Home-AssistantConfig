0ebae1a19b2060120767f428b16163e35ba77605
CCOSTAN <carlo@ipm.com>
2017-12-19 07:46:56 +0000

    Moved static info into header. .now time for bed.

--- script/header.md
+++ script/header.md
@@ -3,7 +3,7 @@
 
 Be sure to :star: my repo so you can keep up to date on the daily progress!
 
-All of my Scripts are in this directory.  These scripts can be used over and over by different automations.  I try to modulize most of my scripts so that they can be passed variables from different automations and still keep all the settings normailized and centralized. 
+All of my Scripts are in this directory.  These scripts can be used over and over by different automations.  I try to modulize most of my scripts so that they can be passed variables from different automations and still keep all the settings normailized and centralized.
 
 #Still have questions on my Config?
 Follow me on twitter : [@CCostan](https://twitter.com/ccostan)
@@ -13,3 +13,8 @@ You can also vist my [Blog](http://www.vmwareinfo.com/search/label/iot) for all 
 <a target='_blank' rel='nofollow' href='https://app.codesponsor.io/link/3MkgeKFUVKPNW45Vtwu9Abd4/CCOSTAN/Home-AssistantConfig'>
   <img alt='Sponsor' width='888' height='68' src='https://app.codesponsor.io/embed/3MkgeKFUVKPNW45Vtwu9Abd4/CCOSTAN/Home-AssistantConfig.svg' />
 </a>
+
+Original mapping work by @beed2112
+Modified by [@CCOSTAN](https://twitter.com/ccostan)
+Repo : https://github.com/beed2112/condo
+This is a map of the script references from my repo.
--- shell_scripts/script_notations.sh
+++ shell_scripts/script_notations.sh
@@ -5,12 +5,6 @@
 
 cat /home/hass/.homeassistant/script/header.md > /home/hass/.homeassistant/script/README.md
 
-echo "Original work by @beed2112 \n" >> /home/hass/.homeassistant/script/README.md
-echo "Modified by [@CCOSTAN](https://twitter.com/ccostan) \n" >> /home/hass/.homeassistant/script/README.md
-echo "Repo : https://github.com/CCOSTAN/Home-AssistantConfig \n" >> /home/hass/.homeassistant/script/README.md
-echo "Repo : https://github.com/beed2112/condo \n" >> /home/hass/.homeassistant/script/README.md
-echo "**This is a map of the script references from my repo.** \n" >> /home/hass/.homeassistant/script/README.md
-
 grep -e '^[a-z]' /home/hass/.homeassistant/script/*.yaml | tr : '\n' > /tmp/scriptnames.txt
 
 sed -i '/^$/d' /tmp/scriptnames.txt
@@ -20,8 +14,8 @@ for item in `cat /tmp/scriptnames.txt`
 do
 
   res=`grep -R script.$item /home/hass/.homeassistant/. --include=*.yaml`
-  echo "$item \n" | sed 's|/home/hass/\.homeassistant/| https://github\.com/CCOSTAN/Home-AssistantConfig/blob/master/|g'>> /home/hass/.homeassistant/script/README.md
-  echo "$res  \n" | sed 's|/home/hass/\.homeassistant/\.| https://github\.com/CCOSTAN/Home-AssistantConfig/blob/master|g' >> /home/hass/.homeassistant/script/README.md
+  echo "\n $item \n" | sed 's|/home/hass/\.homeassistant/| https://github\.com/CCOSTAN/Home-AssistantConfig/blob/master/|g'>> /home/hass/.homeassistant/script/README.md
+  echo "\n $res \n" | sed 's|/home/hass/\.homeassistant/\.| https://github\.com/CCOSTAN/Home-AssistantConfig/blob/master|g' >> /home/hass/.homeassistant/script/README.md
 
 sed -i '/^$/d' /home/hass/.homeassistant/script/README.md 
   
