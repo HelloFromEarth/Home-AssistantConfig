37bc7cc145d16efa5cfcbd640006a1e57e6b6ad5
CCOSTAN <carlo@ipm.com>
2017-12-19 07:15:55 +0000

    More and more awesome updates. :) @Beed2112 - Thanks for the foundation.

--- shell_scripts/script_notations.sh
+++ shell_scripts/script_notations.sh
@@ -3,11 +3,13 @@
 # this script uses hard-coded paths 
 # This deletes the readme file and begins rebuilding it.. 
 
-echo "# Original work by @beed2112" > /home/hass/.homeassistant/script/readme.md
-echo "# Modified by @CCOSTAN" >> /home/hass/.homeassistant/script/readme.md
-echo "# Repo : https://github.com/CCOSTAN/Home-AssistantConfig" >> /home/hass/.homeassistant/script/readme.md
-echo "# Repo : https://github.com/beed2112/condo " >> /home/hass/.homeassistant/script/readme.md
-echo "# This is a map of the script references from my repo. " >> /home/hass/.homeassistant/script/readme.md
+cat /home/hass/.homeassistant/script/header.md > /home/hass/.homeassistant/script/readme.md
+
+echo "Original work by @beed2112" >> /home/hass/.homeassistant/script/readme.md
+echo "Modified by [@CCOSTAN](https://twitter.com/ccostan" >> /home/hass/.homeassistant/script/readme.md
+echo "Repo : https://github.com/CCOSTAN/Home-AssistantConfig" >> /home/hass/.homeassistant/script/readme.md
+echo "Repo : https://github.com/beed2112/condo " >> /home/hass/.homeassistant/script/readme.md
+echo "** This is a map of the script references from my repo. **" >> /home/hass/.homeassistant/script/readme.md
 
 grep -e '^[a-z]' /home/hass/.homeassistant/script/*.yaml | tr : '\n' > /tmp/scriptnames.txt
 
@@ -23,7 +25,7 @@ do
 #  echo "**-------------------------------------------------------------------------**" >> /home/hass/.homeassistant/script/readme.md
   echo "$item " >> /home/hass/.homeassistant/script/readme.md
 #  echo "**-------------------------------------------------------------------------**" >> /home/hass/.homeassistant/script/readme.md
-  echo "$res  " | sed 's|/home/hass/\.homeassistant/\.| https//github\.com/CCOSTAN/Home-AssistantConfig/blob/master|g' >> /home/hass/.homeassistant/script/readme.md
+  echo "$res  " | sed 's|/home/hass/\.homeassistant/\.| https://github\.com/CCOSTAN/Home-AssistantConfig/blob/master|g' >> /home/hass/.homeassistant/script/readme.md
 
 done
 cat /home/hass/.homeassistant/script/readme.md
